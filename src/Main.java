758a1839e7a080cc04caae1efdfd9717d188ddf9
jose jimenez <jimenjos000@warren.k12.in.us>
2024-09-20 13:07:07 -0400

    km/l

--- src/Main.java
+++ src/Main.java
@@ -1,2 +1,41 @@
-package PACKAGE_NAME;public class Main {
+public class Main {
+    public static void main(String[] args) {
+        System.out.println(sumFirstAndLastDigit(252));
+        System.out.println(sumFirstAndLastDigit(257));
+        System.out.println(sumFirstAndLastDigit(0));
+        System.out.println(sumFirstAndLastDigit(5));
+        System.out.println(sumFirstAndLastDigit(-10));
+    }
+
+    private static int sumFirstAndLastDigit(int number) {
+        if (number < 0) {
+            return -1;
+        }
+
+
+
+
+        int Lastdigit = number % 10;
+        //Get the last digit:  mod the number by 10:
+        // store in some variable
+
+        int firstDigit = 0;
+
+         while ( number > 0) {
+             firstDigit = number;
+             number /= 10;
+        } return firstDigit + Lastdigit;
+
+
+
+
+
+
+        // While number > 0
+        // set firstDigit to number
+        // divide equals number by 10
+
+        // return the sum of first and last digits
+
+    }
 }
